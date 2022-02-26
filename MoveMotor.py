3e3b2194e7c1f71e88fe63507eb6c2809e62a2ab
Jack Danos <jackadanos@gmail.com>
2022-02-25 18:56:20 -0700

    created file for motor movement functions

---
+++ MoveMotor.py
@@ -0,0 +1,8 @@
+"""
+This file contains the functions to move the stepper motor. 
+Function inputs:
+direction       "left" or "right"
+speed           integer 1-10 for relative speed of motor
+distance        distance to move in mm 
+"""
+
