5286516fd77e47bb43d62d559414e894f0c230ed
Jack Danos <jackadanos@gmail.com>
2022-02-25 19:15:29 -0700

    started move motor script

--- MoveMotor.py
+++ MoveMotor.py
@@ -6,4 +6,19 @@ speed     integer 1-10 for relative speed of motor
 dist      distance to move in mm 
 """
 
-# Test
+
+class MoveMotor:
+    def __init__(self):
+        # Define pins for motor pulse, direction, and homing switch
+        self.pulsePin = 24
+        self.dirPin = 23
+        self.homePin = 25
+        # Initialize GPIO pins
+        GPIO.setup(self.pulsePin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.dirPin, GPIO.OUT, initial=GPIO.LOW)
+        #GPIO.setup(EN, GPIO.OUT) # sets EN as an output pin
+        # Sets  homePin as an input with a pull-down resistor
+        GPIO.setup(self.homePin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+    def moveMotor(self,dir,speed,dist):
+
