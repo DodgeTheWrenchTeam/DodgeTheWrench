03b33aeeee1abd2d55511a11f4caaad63c87df75
Anthoney Griffith <anthoneyg512@hotmail.com>
2022-03-25 12:10:59 -0600

    muted some annoying test print

--- Avoidance.py
+++ Avoidance.py
@@ -1,10 +1,11 @@
-#Test run for algorithm in determination of projectile avoidance
+# Test run for algorithm in determination of projectile avoidance
 import math
 import time
 
-#defining vector object class
+
+# defining vector object class
 class Vector:
-    #Initializing vector object based on class call
+    # Initializing vector object based on class call
     def __init__(self, x=0, y=0, z=0):
 
         self.x = x
@@ -12,20 +13,20 @@ class Vector:
         self.y = y
 
         self.z = z
-        #identifying the vector's norm as an attribute
+        # identifying the vector's norm as an attribute
         self.norm = math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
-    #Printing the vector
+    # Printing the vector
     def print(self):
 
         print('[', self.x, self.y, self.z, ']')
 
-    #displaying norm value
+    # displaying norm value
     def printNorm(self):
 
         print(self.norm)
 
-    #normalizing the vector and creating a unit vector from it
+    # normalizing the vector and creating a unit vector from it
     def unit(self):
 
         a = self.x / self.norm
@@ -36,7 +37,7 @@ class Vector:
 
         return Vector(a, b, c)
 
-    #eliminating vector component depending on which basis plane you want it projected on
+    # eliminating vector component depending on which basis plane you want it projected on
     def project(self, direction):
 
         if direction == 'x':
@@ -66,7 +67,7 @@ class Vector:
         return Vector(d, e, f)
 
 
-#Subtracting two vectors to determine a current velocity
+# Subtracting two vectors to determine a current velocity
 
 
 # def calculateVelocity(p, q):
@@ -95,7 +96,7 @@ class Vector:
 #
 # x_tol = 10
 
-#Main algorithm for object avoidance determination
+# Main algorithm for object avoidance determination
 
 
 # Inputs:
@@ -103,47 +104,47 @@ class Vector:
 # 1     position 2, defined as a list
 # x_tol tolerance in mm for collision box
 def DodgeWrench(p, q, x_tol):
-    #start time of algorithm for determining algorithm run time
+    # start time of algorithm for determining algorithm run time
     start = time.time()
     # Convert input lists into necessary vectors
     p = Vector(p[0], p[1], p[2])
 
     q = Vector(q[0], q[1], q[2])
 
-    #finding velocity from two measured points
+    # finding velocity from two measured points
     v_net = Vector(q.x - p.x, q.y - p.y, q.z - p.z)
 
     if v_net.norm <= 25 or v_net.z >= 0:
         return "Stay", 0
 
-    v_net.print() ###
+    # v_net.print() ###
 
-    v_net.printNorm() ###
+    # v_net.printNorm() ###
 
-    #projecting onto plane of interest
+    # projecting onto plane of interest
     v_naught = v_net.project('y')
-    #generating unit vector
+    # generating unit vector
     v_hat = v_naught.unit()
 
-    v_hat.print() ###
+    # v_hat.print() ###
 
-    v_hat.printNorm() ###
+    # v_hat.printNorm() ###
 
-    #using dot product to find relative angle
+    # using dot product to find relative angle
     theta = math.acos((-v_hat.z*p.z)/p.z)
 
-    print('theta =', theta) ###
-    #solving for distance from expected point of x-y plane intersection
+    # print('theta =', theta) ###
+    # solving for distance from expected point of x-y plane intersection
     r_contact = p.z/math.cos(theta)
 
-    print('rcontact =', r_contact) ###
+    # print('rcontact =', r_contact) ###
 
-    #quantity used to determine intersection point relative to the oak-D
+    # quantity used to determine intersection point relative to the oak-D
     x_T = math.sqrt((r_contact**2) - (p.z**2))
 
-    print('x_T =', x_T) ###
+    # print('x_T =', x_T) ###
 
-    #defining initial x direction with considerations made for niche cases
+    # defining initial x direction with considerations made for niche cases
     if p.x != 0:
         x_hat = p.x/abs(p.x)
     elif v_naught.x < 0:
@@ -153,13 +154,13 @@ def DodgeWrench(p, q, x_tol):
     else:
         x_hat = 0
 
-    #some tolerance so that the program doesnt break
+    # some tolerance so that the program doesnt break
     epsilon = 0.001
 
-    #initializing x* so that program is happy
+    # initializing x* so that program is happy
     x_star = 0
 
-    #checking possible cases to determine proper way to calculate x*
+    # checking possible cases to determine proper way to calculate x*
     if abs(v_naught.x) < epsilon:
 
         x_star = p.x
@@ -178,7 +179,7 @@ def DodgeWrench(p, q, x_tol):
 
     print('x* =', x_star) ###
 
-    #determining choice based on value of x* and desired collision window
+    # determining choice based on value of x* and desired collision window
     if abs(x_star) > x_tol:
 
         Choice = 'Stay'
@@ -199,11 +200,12 @@ def DodgeWrench(p, q, x_tol):
 
         Choice = 'Error'
 
-    #How far does the cart need to move to avoid the projectile?
+    # How far does the cart need to move to avoid the projectile?
     moveDistance = x_tol - abs(x_star)
     runTime = time.time() - start
     print('Run Time =', runTime)
     return Choice, moveDistance
+
 
 if __name__ == "__main__":
     p = [100,0,500]
